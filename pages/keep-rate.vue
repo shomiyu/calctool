8fb18a67913aeea1f2ceb5ed395cd5b83ac81535
miyuki shoji <0428.miyuki.shoji@gmail.com>
2021-03-17 20:03:32 +0900

    🎉縦横比を保持して可変ページを追加

---
+++ pages/keep-rate.vue
@@ -0,0 +1,75 @@
+<template>
+  <article class="calcContents">
+    <h2 class="titlePrimary">縦横比を保持したまま可変</h2>
+    <!-- 縦横比 -->
+    <section class="calcSection">
+      <h3 class="calcSection__title">padding-top を求める</h3>
+      <CalcBoard
+        :calc-data="paddingTop"
+        @onInput="handleChangeVal($event, 'paddingTop')"
+      />
+    </section>
+  </article>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      paddingTop: [
+        {
+          id: 0,
+          type: 'arg',
+          introduction: '要素/画像の幅が',
+          val: 300,
+          defaultNum: 300,
+          endLabel: 'pxで',
+        },
+        {
+          id: 1,
+          type: 'arg',
+          introduction: '要素/画像の高さが',
+          val: 200,
+          defaultNum: 200,
+          endLabel: '%なら',
+        },
+        {
+          id: 2,
+          type: 'answer',
+          introduction: '設定値は',
+          val: Number,
+          unit: '%',
+        },
+      ],
+    }
+  },
+
+  created() {
+    const paddingTopAnswer = this.paddingTop[1].val / this.paddingTop[0].val
+    this.paddingTop[2].val = Math.round(paddingTopAnswer * 10000) / 100
+  },
+
+  methods: {
+    /**
+     * 計算
+     */
+    handleChangeVal(args, type) {
+      if (type === 'paddingTop') {
+        // 入力のあったデータを更新
+        this.paddingTop[args.id].val = args.argNum
+
+        // 再計算
+        const width = this.paddingTop[0].val
+        const height = this.paddingTop[1].val
+        const answer = height / width
+
+        // 小数点の調整
+        this.paddingTop[2].val = Math.round(answer * 10000) / 100
+        return this.paddingTop[2].val
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
